b2bb7b7f37620d878ff8fa61d20649a440b9f83f
Your Name <you@example.com>
2023-09-25 16:02:19 -0300

    agregar proyecto de dimarcos

--- convertidor.py
+++ convertidor.py
@@ -1,30 +1,95 @@
 """1) Módulo temperatura.py: Este módulo debe contener funciones para convertir entre diferentes unidades de temperatura, como Celsius, Fahrenheit y Kelvin.(0.5 puntos) """
 # Importa los módulos necesarios
 import temperatura
-
+import masa
+import tiempo
 def main():
     while True:
         # Muestra el menú principal
         print("Menú Principal:")
         print("[1]Convertir de celcius a fahrenheit")
-        print("[2] Da vuelta tu nombre")
-        print("[3] Adivina un número en 1 y 10")
-        print("[0] Salir")
-        
-        # Solicita al usuario que ingrese una opción
-        opcion = input("Ingrese una opción: ")
+        print("[2] convertir de celcius a kelvin")
+        print("[3] convertir de fahrenheit a celcius ")
+        print("[4] convertir de fahrenheit a kelvin")
+        print("[5] convertir de kelvin a celcius")
+        print("[6] convertir de kelvin a fahrenheit")
+        print("[7] convertir de kilogramos a gramos ")
+        print("[8] convertir kilogramos a toneladas")
+        print("[9] convertir gramos a kilogramos")
+        print("[10] convertir gramos a toneladas")
+        print("[11] convertir toneladas a gramos")
+        print("[12] convertir toneladas a kilogramos")
+        print("[13] convertir segundos a minutos")
+        print("[14] convertir segundos a horas")
+        print("[15] convertir minutos a segundos")
+        print("[16] convertir minutos a horas")
+        print("[17] convertir horas a segundos")
+        print("[18] convertir horas a minutos")
+        print("[0] Salir del programa")
 
+    # Solicita al usuario que ingrese una opción
+        opcion = input("Ingrese una opción: ")
+        valorinicial= int(input('ingrese valor inicial'))
         try:
             opcion = int(opcion)
             if opcion == 0:
                 print("Saliendo del programa. ¡Hasta luego!")
                 break
             elif opcion == 1:
-                calcular_edad()
-            elif opcion == 2:
-                dar_vuelta_nombre()
-            elif opcion == 3:
-                adivinar_numero()
+              resultado= temperatura.celsius_a_fahrenheit(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= temperatura.celsius_a_kelvin(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= temperatura.fahrenheit_a_celsius(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= temperatura.fahrenheit_a_kelvin(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= temperatura.kelvin_a_celsius(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= temperatura.kelvin_a_fahrenheit(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= masa.kg_a_gr(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= masa.kg_a_ton(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= masa.gr_a_kg(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= masa.gr_a_ton(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= masa.ton_a_gr(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= masa.ton_a_kg(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= tiempo.sec_a_min(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= tiempo.sec_a_hr(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= tiempo.min_a_sec(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= tiempo.min_a_hr(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= tiempo.hr_a_sec(valorinicial)
+              print("El resultado es: ", resultado)
+            elif opcion == 1:
+              resultado= tiempo.hr_a_min(valorinicial)
+              print("El resultado es: ", resultado)
+
             else:
                 print("Opción no válida. Por favor, ingrese una opción válida.")
         except ValueError:
