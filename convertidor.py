1ccb2da7127d3cc32dd1656f1789bc5f949ba250
Your Name <you@example.com>
2023-09-25 15:05:56 -0300

    Modulo tiempo creado

--- convertidor.py
+++ convertidor.py
@@ -1,4 +1,37 @@
 """1) Módulo temperatura.py: Este módulo debe contener funciones para convertir entre diferentes unidades de temperatura, como Celsius, Fahrenheit y Kelvin.(0.5 puntos) """
+# Importa los módulos necesarios
+import temperatura
+
+def main():
+    while True:
+        # Muestra el menú principal
+        print("Menú Principal:")
+        print("[1]Convertir de celcius a fahrenheit")
+        print("[2] Da vuelta tu nombre")
+        print("[3] Adivina un número en 1 y 10")
+        print("[0] Salir")
+        
+        # Solicita al usuario que ingrese una opción
+        opcion = input("Ingrese una opción: ")
+
+        try:
+            opcion = int(opcion)
+            if opcion == 0:
+                print("Saliendo del programa. ¡Hasta luego!")
+                break
+            elif opcion == 1:
+                calcular_edad()
+            elif opcion == 2:
+                dar_vuelta_nombre()
+            elif opcion == 3:
+                adivinar_numero()
+            else:
+                print("Opción no válida. Por favor, ingrese una opción válida.")
+        except ValueError:
+            print("Solo puede ingresar valores numéricos.")
+
+if __name__ == "__main__":
+    main()
 """2) Módulo masa.py: Este módulo debe contener funciones para convertir entre diferentes unidades de masa, como kilogramos, libras y onzas.(0.5 puntos) """
 """3) Crear el primer versionamiento con git de los dos módulos creados de temperatura y masa (usar git add y git commit ). (0.5 puntos) """
 """4) Crear una nueva rama llamada “desarrollo” y en esta nueva rama agregar los módulos tiempo.py y convertidor.py (recuerda usar git branch ). (0.5 puntos) """
--- tiempo.py
+++ tiempo.py
@@ -1,1 +1,28 @@
 """5) Módulo tiempo.py: Este módulo debe contener funciones para convertir entre diferentes unidades de tiempo, como segundos, minutos y horas. (1 punto) """
+def sec_a_min(sec):
+    min= sec/60
+    return min
+def sec_a_hr(sec):
+    hr= sec/3600
+    return hr
+def min_a_sec(min):
+    sec= min*60
+    return sec
+def min_a_hr(min):
+    hr= min/60
+    return hr
+def hr_a_sec(hr):
+    sec= hr*3600
+    return sec
+def hr_a_min(hr):
+    min= hr*60
+    return min
+
+if  __name__ == "__main__":
+    print("Ejemplos de conversión de masa:")
+    print("12 kilogramos a gramos:", sec_a_min(500))
+    print("25000 kilogramos a toneladas:", sec_a_hr(10000))
+    print("12000 gramos a kilogramos", min_a_sec(12))
+    print("200000 gramos a toneladas:", min_a_hr(230))
+    print("3 toneladas a kilogramos:", hr_a_sec(3))
+    print("2 toneladas a gramos:", hr_a_min(2))
